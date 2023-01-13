69909283263f23f5b3cffb1e5f4a98bb3a62c6bb
princeutthunga <prince.r@utthunga.com>
2023-01-13 14:23:07 +0530

    groovy updatenew

--- vars/lvBuild.groovy
+++ vars/lvBuild.groovy
@@ -5,5 +5,5 @@ def call(lvProjectPath, targetName, buildSpecName, lvVersion, lvBitness) {
 	echo 'Running LabVIEW build spec on project'
 
 	bat "python -u \"${stepsDir}\\labview_build.py\" \"${projectPath}\" \"${targetName}\" \"${buildSpecName}\" ${lvVersion} ${lvBitness}"
-	bat "C:/Program Files (x86)/National Instruments/Shared/LabVIEW CLI/LabVIEWCLI"
+	
 }
